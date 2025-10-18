425e41c2f3349bb5230582f2e4e3b8eec0ea66cf
Themis <idonotwantspams@gmail.com>
2025-10-19 00:19:09 +0100

    Add placeholder for BALL_UUID in chronoball constants
    @to-do: replace the UUID from the compendium

--- src/chronoball-constants.mjs
+++ src/chronoball-constants.mjs
@@ -3,4 +3,5 @@ export default class Constants {
   static MODULE_NAME = 'Chronoball';
   static TEAM_FLAG = 'team';
   static THROW_FLAVOR = 'Attempting to throw the ball, using';
+  static BALL_UUID = '' // @to-do: replace the UUID from the compendium
 }
