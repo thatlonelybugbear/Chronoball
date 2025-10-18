f86d16875d8affec26d59430df03679ab89b828e
Themis <idonotwantspams@gmail.com>
2025-10-18 20:02:26 +0100

    Add TEAM_FLAG and THROW_FLAVOR constants

--- src/chronoball-constants.mjs
+++ src/chronoball-constants.mjs
@@ -1,4 +1,6 @@
 export default class Constants {
   static MODULE_ID = 'chronoball';
   static MODULE_NAME = 'Chronoball';
+  static TEAM_FLAG = 'team';
+  static THROW_FLAVOR = 'Attempting to throw the ball, using';
 }
