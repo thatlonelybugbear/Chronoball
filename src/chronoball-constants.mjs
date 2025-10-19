49fa53ef99f6bed2ed9a9ca6ce1affb1a853943b
Themis <idonotwantspams@gmail.com>
2025-10-19 02:29:27 +0100

    Update chronoball-constants.mjs

--- src/chronoball-constants.mjs
+++ src/chronoball-constants.mjs
@@ -3,6 +3,6 @@ export default class Constants {
   static MODULE_NAME = 'Chronoball';
   static TEAM_FLAG = 'team';
   static THROW_FLAVOR = 'Attempting to throw the ball, using';
-  static BALL_UUID = '' // @to-do: replace the UUID from the compendium
+  static BALL_UUID = 'Compendium.chronoball.Items.Item.ZOZPuMztAXtgP6Pk';
   static DIALOG_RENDERED = 'renderedDialog';
 }
