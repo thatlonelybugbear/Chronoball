b55cab92254e2edef63b19ee9aee7de63a42a4e9
Themis <idonotwantspams@gmail.com>
2025-10-19 03:32:26 +0100

    Refactor chronoball actions for improved functionality

--- src/chronoball-actions.mjs
+++ src/chronoball-actions.mjs
@@ -5,9 +5,11 @@ import { transferEverything, createItemPile, giveItem, addItems } from './chrono
 const settings = new Settings();
 
 export async function createDialog() {
+	await game.user.setFlag(Constants.MODULE_ID, Constants.DIALOG_RENDERED, true);
 	const { createFormGroup, createNumberInput } = foundry.applications.fields;
 
 	const actor = game.user.character || canvas.tokens.controlled?.[0]?.actor;
+	const token = actor?.token?.object ?? actor?.getActiveTokens?.()?.[0];
 	const isCarrier = actor?.items.find((i) => i.identifier === 'chronoball');
 
 	const buttons = [
@@ -30,24 +32,34 @@ export async function createDialog() {
 		rejectClose: false,
 		id: 'chronoball',
 	});
-	if (!result) console.log('closed');
+	if (!result) {
+		console.log('closed');
+		await game.user.setFlag(Constants.MODULE_ID, Constants.DIALOG_RENDERED, false);
+	}
 	else {
 		if (result === 'create') await createBall();
-		else if (result === 'drop') await dropBall({ actor });
+		else if (result === 'drop') await dropBall({spot: {center: {x: token.x, y: token.y}}, actor });
 		else if (result === 'pick') await pickBall(actor);
 		else if (result === 'throw') await throwBall({ actor, token: actor.getActiveTokens()[0] });
-		createDialog();
+		//createDialog();
 	}
 }
 
 async function createBall() {
 	const ballTokenUuid = canvas.tokens.placeables.find((t) => t.identifier === 'chronoball')?.document.uuid;
 	const spot = { center: { x: 1800, y: 3200 } };
-	if (!ballTokenUuid) return dropBall({ spot });
+	if (!ballTokenUuid) return dropBall({ spot, create: true });
 }
 
-async function dropBall({ spot, actor }) {
-	const ballItem = actor?.items.find((i) => i.identifier === 'chronoball') ?? (await fromUuid(Constants.BALL_UUID));
+async function dropBall({ spot, actor, create = false }) {
+	let ballItem;
+	if (create) {
+		for (const token of canvas.tokens.placeables) await token.actor?.items.find((i) => i.identifier === 'chronoball')?.delete();
+		ballItem = await fromUuid(Constants.BALL_UUID);
+	}
+	else {
+		ballItem = actor?.items.find((i) => i.identifier === 'chronoball');
+	}
 	const ballItemData = game.items.fromCompendium(ballItem);
 	const ownership = game.users.map((u) => ({ [u.id]: 3 }));
 	const options = {
@@ -61,7 +73,8 @@ async function dropBall({ spot, actor }) {
 		pileSettings: { type: game.itempiles.pile_types.PILE },
 	};
 	await createItemPile(options);
-	return;
+	if (ballItem?.actor) return ballItem?.delete();
+	else return;	
 }
 
 async function giveBall({ target, source }) {
@@ -70,8 +83,8 @@ async function giveBall({ target, source }) {
 	await item?.delete();
 }
 
-async function pickUpBall(actor) {
-	const ballTokenUuid = canvas.tokens.placeables.find((t) => t.identifier === 'chronoball')?.document.uuid;
+async function pickBall(actor) {
+	const ballTokenUuid = canvas.tokens.placeables.find(t=>t.name==='Chronoball')?.document.uuid;
 	await transferEverything(ballTokenUuid, actor);
 }
 
@@ -79,6 +92,7 @@ async function throwBall({ actor, token }) {
 	const ballItem = actor.items.find((i) => i.identifier === 'chronoball');
 	const ballItemData = game.items.fromCompendium(ballItem);
 	const interceptionRange = settings.interceptionRange;
+	console.log(interceptionRange)
 	const interceptionDistance = interceptionRange * canvas.grid.size;
 	const findNearby = canvas.tokens.quadtree.getObjects(token.bounds.pad(interceptionDistance - 1)).filter((t) => t !== token && t?.actor?.getFlag(Constants.MODULE_ID, Constants.TEAM_FLAG) !== actor.getFlag(Constants.MODULE_ID, Constants.TEAM_FLAG));
 	if (findNearby.length) await interceptBall({ token, findNearby });
@@ -86,9 +100,11 @@ async function throwBall({ actor, token }) {
 	const skill = actor.getRollData().skills.ath.total > actor.getRollData().skills.slt.total ? 'ath' : 'slt';
 
 	const throwRoll = await actor.rollSkill({ skill }, {}, { flavor: Constants.THROW_FLAVOR + ` ${skill === 'ath' ? 'Athletics' : 'Sleight of Hands'}` });
-	const total = throwRoll.total;
+	const total = throwRoll[0].total;
 	const rangeFormula = settings.throwFormula;
-	const limitMaxRange = (throwRoll < rangeFormula.baseDC ? 0 : Math.floor((throwRoll - rangeFormula.baseDC) / rangeFormula.modifier)) * 2 * canvas.grid.distance;
+	const limitMaxRange = (total < rangeFormula.baseDC ? 0 : Math.floor((total - rangeFormula.baseDC) / rangeFormula.modDC)) * 2 * canvas.grid.distance;
+	ui.notifications.info(`You can throw ${limitMaxRange}ft away`);
+	console.log(total, total-Math.floor((total - rangeFormula.baseDC) / rangeFormula.modDC))
 
 	const crosshairOptions = {
 		texture: ballItemData.img,
@@ -102,11 +118,14 @@ async function throwBall({ actor, token }) {
 	const isTeamate = Array.from(canvas.tokens.quadtree.getObjects(clonedToken.object.bounds.pad(-1))).filter((t) => t?.actor?.getFlag(Constants.MODULE_ID, Constants.TEAM_FLAG) === actor.getFlag(Constants.MODULE_ID, Constants.TEAM_FLAG) && t.id !== token.id);
 
 	if (isTeamate.length) {
+		ui.notifications.info(`${isTeamate[0].name} catches the ball`);
 		return giveBall({ target: isTeamate[0], source: actor });
 	} else {
 		const spot = { center: { x: crosshair.x, y: crosshair.y } };
-		return dropBall({ spot });
+		return dropBall({ spot, actor });
 	}
 }
 
-async function interceptBall({ token, findNearby }) {}
+async function interceptBall({ token, findNearby }) {
+	ui.notifications.info('someone can interceptBall');
+}
